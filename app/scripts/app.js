cbfa2d5e293c0d17325e1d34d3d7a0410d675d56
Mathieu Elie <mathieuelie@gmail.com>
2012-12-29 11:14:21 +0100

    Initial commi

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var yeomanD3jsHelloWorldApp = angular.module('yeomanD3jsHelloWorldApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+yeomanD3jsHelloWorldApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
