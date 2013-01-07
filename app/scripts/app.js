0f0f3ecdd984ae686a5117bed5630a5087e2ca65
Mathieu Elie <mathieuelie@gmail.com>
2013-01-07 10:55:50 +0100

    more modular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var yeomanD3jsHelloWorldApp = angular.module('yeomanD3jsHelloWorldApp', [])
+var yeomanD3jsHelloWorldApp = angular.module('yeomanD3jsHelloWorldApp', ['ghD3'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
   .when('/', {
@@ -11,57 +11,4 @@ var yeomanD3jsHelloWorldApp = angular.module('yeomanD3jsHelloWorldApp', [])
     redirectTo: '/'
   });
 }]);
-
-yeomanD3jsHelloWorldApp.directive('ghD3', function () {
-
-  // constants
-  var margin = 20,
-  width = 960,
-  height = 500 - .5 - margin;
-
-  return {
-    restrict: 'E',
-    terminal: true,
-    scope: {
-      val: '='
-    },
-
-    link: function (scope, element, attrs) {
-
-      var vis = d3.select(element[0])
-      .append("svg")
-      .attr("class", "chart")
-      .attr("width", width)
-      .attr("height", height + margin + 100);
-      
-      // whenever the bound 'exp' expression changes, execute this 
-      scope.$watch('val', function (newVal, oldVal) {
-
-        // clear
-        vis.selectAll('*').remove();
-
-        // if 'val' is undefined, exit
-        if (!newVal) {
-          return;
-        }
-        
-        // get data from scope
-        var data = newVal;
-
-        // display
-        var x = d3.scale.linear()
-        .domain([0, d3.max(data)])
-        .range([0, 420]);
-
-        vis.selectAll("rect")
-          .data(data)
-          .enter().append("rect")
-          .attr("y", function(d, i) { return i * 20; })
-          .attr("width", x)
-          .attr("height", 20);
-        // display done..
-
-      });
-    }
-  };
-});
+;
